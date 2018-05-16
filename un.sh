295a3d4d2d883d065b5def2453071e0e493a305f
Max Mak <maxmak@maxsmak.com>
2018-05-16 22:32:17 +0800

    uninstall sh

---
+++ un.sh
@@ -0,0 +1,13 @@
+
+sudo rm /usr/local/sbin/kamailio
+sudo rm /usr/local/sbin/kamctl
+sudo rm /usr/local/sbin/kamdbctl
+sudo rm /usr/local/sbin/kamcmd
+sudo rm /usr/local/man/man5/kamailio.cfg.5
+sudo rm /usr/local/man/man8/kamailio.8
+sudo rm /usr/local/man/kamctl.8
+sudo rm /usr/local/man/kamdbctl.8
+sudo rm -rf /usr/local/etc/kamailio/
+sudo rm -rf /usr/local/share/doc/kamailio/
+sudo rm -rf /usr/local/lib64/kamailio/
+sudo rm -rf /usr/local/share/kamailio/
